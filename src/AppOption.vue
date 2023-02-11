d41081524a2adcdd20b8096ec2e71adde00cb512
Jon Dimaculangan <jondmclngn@gmail.com>
2023-02-11 01:41:47 -0600

    use state (ref)

---
+++ src/AppOption.vue
@@ -0,0 +1,24 @@
+<script>
+// Options API
+export default {
+    data() {
+        return {
+            name: 'Jon'
+        }
+    },
+    computed: {
+        greetings() {
+            if (!this.name.length) {
+                return 'Who are you?'
+            }
+            return `Hello ${this.name}`
+        }
+    }
+}
+
+</script>
+
+<template>
+    <input type="text" v-model="name" />
+    <h1>Hello, {{ name }}</h1>
+</template>
